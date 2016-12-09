7735ae8f3197c772946bd1f3258a99eac9699b36
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2016-12-09 22:19:51 +0000

    More model fields

--- subscribae/models.py
+++ subscribae/models.py
@@ -7,6 +7,8 @@ class Subscription(models.Model):
     """A subscription that belongs to a user"""
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
     channel_id = models.CharField(max_length=200)  # check this?
+    last_update = models.DateTime()
+    last_viewed = models.DateTime()
 
 
 class Bucket(models.Model):
@@ -15,8 +17,14 @@ class Bucket(models.Model):
     A subscription can be in more than one bucket
     """
     subs = RelatedSetField(Subscription)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    last_update = models.DateTime()
+    last_viewed = models.DateTime()
 
 
 class Video(models.Model):
     """A video"""
     subs = models.ForeignKey(Subscription)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    viewed = models.BooleanField(default=False)
+    youtube_id = models.CharField(max_length=200)  # check this?
