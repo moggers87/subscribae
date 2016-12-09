6911ecb33f33ed11a1aae017cf4236d6e9daa2c4
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2016-12-09 12:54:09 +0000

    Initial commit

---
+++ manage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+import sys
+
+from subscribae.boot import fix_path
+fix_path(include_dev_libs_path=True)
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "subscribae.settings")
+
+    from djangae.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
