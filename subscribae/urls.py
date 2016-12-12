77faf1f36e1ab7e1801ed1306a58787e45bfab1a
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2016-12-12 21:46:14 +0000

    Clear up URLs

--- subscribae/urls.py
+++ subscribae/urls.py
@@ -20,9 +20,6 @@ urlpatterns = (
 
     url(r'^_ah/', include('djangae.urls')),
 
-    # Note that by default this is also locked down with login:admin in app.yaml
-    url(r'^admin/', include(admin.site.urls)),
-
     url(r'^csp/', include('cspreports.urls')),
 
     url(r'^auth/', include('djangae.contrib.gauth.urls')),
