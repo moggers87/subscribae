35ac12dce7b7d9c3641864057638735d8b93c5ad
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2016-12-22 16:25:35 +0000

    WIP

--- subscribae/utils.py
+++ subscribae/utils.py
@@ -1,9 +1,18 @@
 import os
 
+from apiclient.discovery import build
 from django.conf import settings
 from django.core.urlresolvers import reverse
-
+from google.appengine.ext.deferred import deferred
+from google.appengine.runtime import DeadlineExceededError as RuntimeExceededError
 from oauth2client import client
+import httplib2
+
+from subscribae.models import OauthToken
+
+
+API_NAME = 'youtube'
+API_VERSION = 'v3'
 
 
 def get_oauth_flow(user):
@@ -18,3 +27,24 @@ def get_oauth_flow(user):
     flow.params['access_type'] = 'offline'
 
     return flow
+
+
+def import_data_for_user(user_id, page_token=None):
+    try:
+        youtube = get_service(user_id)
+        subscription_list = youtube.subscriptions() \
+            .list(mine=True, part='sniipet, contentDetails', pageToken=page_token) \
+            .execute()
+        for item in subscription_list:
+            print item
+    except RuntimeExceededError:
+        deferred.defer(import_data_for_user, user_id, page_token)
+
+
+def get_service(user_id):
+    token = OauthToken.objects.get(user_id=user_id)
+    credentials = token.get()
+    http = credentials.authorize(httplib2.Http())
+
+    service = build(API_NAME, API_VERSION, http=http)
+    return service
