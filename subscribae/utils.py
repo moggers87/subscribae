b9573abe016995cbf0ebdd963e3b610b5eb0bb8e
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2016-12-23 00:47:57 +0000

    Import subscriptions

--- subscribae/utils.py
+++ subscribae/utils.py
@@ -3,16 +3,19 @@ import os
 from apiclient.discovery import build
 from django.conf import settings
 from django.core.urlresolvers import reverse
+from django.utils import timezone
 from google.appengine.ext.deferred import deferred
 from google.appengine.runtime import DeadlineExceededError as RuntimeExceededError
 from oauth2client import client
 import httplib2
 
-from subscribae.models import OauthToken
+from subscribae.models import Subscription, OauthToken, create_composite_key
+
 
 
 API_NAME = 'youtube'
 API_VERSION = 'v3'
+API_MAX_RESULTS = 10
 
 
 def get_oauth_flow(user):
@@ -32,11 +35,45 @@ def get_oauth_flow(user):
 def import_data_for_user(user_id, page_token=None):
     try:
         youtube = get_service(user_id)
-        subscription_list = youtube.subscriptions() \
-            .list(mine=True, part='sniipet, contentDetails', pageToken=page_token) \
-            .execute()
-        for item in subscription_list:
-            print item
+        while True:
+            subscriptions = {}
+
+            subscription_list = youtube.subscriptions() \
+                .list(mine=True, part='snippet', maxResults=API_MAX_RESULTS, pageToken=page_token) \
+                .execute()
+
+            for item in subscription_list['items']:
+                channel_id = item['snippet']['resourceId']['channelId']
+
+                subscriptions[channel_id] = dict(
+                    id=create_composite_key(str(user_id), channel_id),
+                    user_id=user_id,
+                    last_update=timezone.now(),
+                    channel_id=channel_id,
+                    title=item["snippet"]["title"],
+                    description=item["snippet"]["description"],
+                    upload_playlist=None,  # must fetch this from the channel data
+                )
+
+            channel_list = youtube.channels() \
+                .list(id=",".join(subscriptions.keys()), part='contentDetails', maxResults=API_MAX_RESULTS) \
+                .execute()
+
+            sub_size = len(subscription_list['items'])
+            chan_size = len(channel_list['items'])
+            assert sub_size == chan_size, "Subscription list and channel list are different sizes! (%s != %s)" % (sub_size, chan_size)
+
+            for channel in channel_list['items']:
+                subscriptions[channel["id"]]["upload_playlist"] = channel["contentDetails"]["relatedPlaylists"]["uploads"]
+
+            for sub in subscriptions.itervalues():
+                key = sub.pop("id")
+                Subscription.objects.update_or_create(id=key, defaults=sub)
+
+            if 'nextPageToken' in subscription_list:
+                page_token = subscription_list['nextPageToken']
+            else:
+                break
     except RuntimeExceededError:
         deferred.defer(import_data_for_user, user_id, page_token)
 
