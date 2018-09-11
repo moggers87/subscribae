d64b169a58ec93f4a761639432570ba7f0039502
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2018-09-11 16:33:55 +0100

    Test that published_at is saved correctly

--- subscribae/tests/test_import_video.py
+++ subscribae/tests/test_import_video.py
@@ -16,10 +16,13 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ##
 
+from datetime import datetime
+
 from djangae.test import TestCase
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 from google.appengine.runtime import DeadlineExceededError as RuntimeExceededError
+from pytz import UTC
 import mock
 
 from subscribae.models import Subscription, OauthToken, Video
@@ -86,9 +89,11 @@ class ImportVideoTasksTestCase(TestCase):
         video1 = Video.objects.get(youtube_id="video123")
         self.assertEqual(video1.title, "my video")
         self.assertEqual(video1.description, "this is my video")
+        self.assertEqual(video1.published_at, datetime(1997, 7, 16, 19, 20, 30, 450000, tzinfo=UTC))
         video2 = Video.objects.get(youtube_id="video456")
         self.assertEqual(video2.title, "my other video")
         self.assertEqual(video2.description, "this is my other video")
+        self.assertEqual(video2.published_at, datetime(1997, 7, 16, 19, 20, 30, 450000, tzinfo=UTC))
 
     @mock.patch('subscribae.utils.get_service')
     def test_import_videos_pagination(self, service_mock):
@@ -244,9 +249,11 @@ class ImportVideoTasksTestCase(TestCase):
         video1 = Video.objects.get(youtube_id="video123")
         self.assertEqual(video1.title, "my video")
         self.assertEqual(video1.description, "this is my video")
+        self.assertEqual(video1.published_at, datetime(1997, 7, 16, 19, 20, 30, 450000, tzinfo=UTC))
         video2 = Video.objects.get(youtube_id="video456")
         self.assertEqual(video2.title, "")
         self.assertEqual(video2.description, "")
+        self.assertNotEqual(video2.published_at, datetime(1997, 7, 16, 19, 20, 30, 450000, tzinfo=UTC))
 
     def test_missing_oauth_token(self):
         user = get_user_model().objects.create(username='1')
